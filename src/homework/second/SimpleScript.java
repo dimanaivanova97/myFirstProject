651c00bf040d727ab631237bc1ad0e644c375be0
dimana.ivanova <dimana.ivanova@mentormate.com>
2022-04-07 11:07:02 +0300

    Logic change of the SimpleScript

--- src/homework/second/SimpleScript.java
+++ src/homework/second/SimpleScript.java
@@ -3,7 +3,21 @@ package homework.second;
 public class SimpleScript {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        int a = 0;
+        int b = 1;
+        int c = 2;
 
+        a *= c;
+        int aEualsB = 0;
+
+        if (a < b && a < c){
+            System.out.println("A is the smallest number");
+        } else if (b < a && b < c){
+            System.out.println("B is the smallest number");
+        } else if (c < a && c < b){
+            System.out.println("C is the smallest number");
+        } else {
+            System.out.println("There are numbers that are equal");
+        }
     }
 }
