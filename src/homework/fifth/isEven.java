9e31f26478c387da3c78fa0a65fc450c26224682
dimana.ivanova <dimana.ivanova@mentormate.com>
2022-04-14 23:01:54 +0300

    Method returns true if the argument is even, or false otherwise.

---
+++ src/homework/fifth/isEven.java
@@ -0,0 +1,29 @@
+package homework.fifth;
+
+import java.util.Scanner;
+
+public class isEven {
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+
+        int inputNum = myScanner.nextInt();
+
+        if (isEven(inputNum)){
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+
+    }
+    boolean isEven(int input){
+        boolean isItEven = true;
+        if (input % 2 != 0){
+            isItEven = false;
+        }
+        return isItEven;
+    }
+
+}
